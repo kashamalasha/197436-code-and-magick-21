c797edd3d0a972aa4f7a6306a77a76000dec220c
kashamalasha <dmitry.burnyshev@gmail.com>
2020-10-02 22:15:18 +0300

    Вносит правки в color.js: - заменяет подбор цвета по element.getComputedStyle() на obj.input.value

--- js/color.js
+++ js/color.js
@@ -2,62 +2,12 @@
 
 (function () {
 
-  const REGEX_RGB = /^rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)$/i;
-  const REGEX_HEX = /^#([0-9A-F]{3}){1,2}$/i;
-
-  const Color = {
-    BLACK: {
-      rgb: `rgb(0, 0, 0)`
-    },
-    RED: {
-      rgb: `rgb(255, 0, 0)`
-    },
-    BLUE: {
-      rgb: `rgb(0, 0, 255)`
-    },
-    YELLOW: {
-      rgb: `rgb(255, 255, 0)`
-    },
-    GREEN: {
-      rgb: `rgb(0, 128, 0)`
-    },
-    fromId(id) {
-      return this[id.toUpperCase()];
-    }
-  };
-
-  let hexToRGB = function (hex) {
-    let r = `0x` + hex[1] + hex[2];
-    let g = `0x` + hex[3] + hex[4];
-    let b = `0x` + hex[5] + hex[6];
-
-    return `rgb(` + +r + `, ` + +g + `, ` + +b + `)`;
-  };
-
-  let getCurrentColor = function (element) {
-    if (element.tagName.toLowerCase() === `div`) {
-      return window.getComputedStyle(element).backgroundColor;
-    } else {
-      return window.getComputedStyle(element).fill;
-    }
-  };
-
-  let setNewColor = function (element, colors) {
+  let setNewColor = function (obj, colors) {
     let newColor = window.util.getRandomFromArray(colors);
-    let isSimilar = false;
-    let currentColor = getCurrentColor(element);
+    let currentColor = obj.input.value;
 
-    if (REGEX_HEX.test(newColor)) {
-      isSimilar = (currentColor === hexToRGB(newColor));
-    } else if (!REGEX_RGB.test(newColor)) {
-      isSimilar = (currentColor === Color.fromId(newColor).rgb);
-    } else {
-      isSimilar = (currentColor === newColor);
-    }
-
-    while (isSimilar) {
-      newColor = setNewColor(element, colors);
-      isSimilar = false;
+    while (newColor === currentColor) {
+      newColor = setNewColor(obj, colors);
     }
 
     return newColor;
@@ -65,7 +15,7 @@
 
   let colorize = function (obj, colors) {
     obj.element.addEventListener(`click`, function () {
-      let newColor = setNewColor(obj.element, colors);
+      let newColor = setNewColor(obj, colors);
       if (obj.element.tagName.toLowerCase() === `div`) {
         obj.element.style.backgroundColor = newColor;
       } else {
